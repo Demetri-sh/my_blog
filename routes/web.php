b38c9ca658ef03041224f5b2802832915c51ac34
Demetri-sh <demetrishamoun@hotmail.com>
2025-07-21 12:28:08 +0300

    Add PageController with home, about, contact methods

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function() {
+    return view('Hello World');
+});
+
+Route::view('/', 'home');
+Route::view('/about', 'about');
+Route::view('/contact', 'contact');
+
+use App\Http\Controllers\PageController;
+
+Route::get('/', [PageController::class, 'home']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/contact', [PageController::class, 'contact']);
+
+Route::get('/user/{id}', function ($id) {
+    return "User ID: $id";
+});
+
+Route::get('/greet/{name?}', function ($name = 'Guest') {
+    return "Hello, $name!";
+});
+
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
