bde1bcaa0baab137130e4cca1cb635043478f449
Demetri-sh <demetrishamoun@hotmail.com>
2025-07-21 13:58:18 +0300

    Add PageController with home, about, contact methods

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 
-
-Route::get('/test', function() {
+Route::get('/test', function () {
     return view('Hello World');
 });
 
